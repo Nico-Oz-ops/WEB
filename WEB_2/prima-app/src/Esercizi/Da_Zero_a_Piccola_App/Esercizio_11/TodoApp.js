3cde2990d6736e129f4da29cad736f74327d4cd5
Nico Valenzuela <nicolasvr1988@gmail.com>
2025-09-17 08:33:48 +0200

    Update 3

---
+++ WEB_2/prima-app/src/Esercizi/Da_Zero_a_Piccola_App/Esercizio_11/TodoApp.js
@@ -0,0 +1,9 @@
+/*
+Esercizio 11: To-Do List con Persistenza (json-server)
+
+Obiettivo: Creare un'applicazione To-Do List completa che interagisce con un backend
+fittizio (json-server) per caricare, aggiungere, eliminare e aggiornare le attività, rendendo
+i dati persistenti.
+*/
+
+import React from "react";
